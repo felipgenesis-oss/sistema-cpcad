806f74f963dc361f49c4afb8aaae0409c3f213eb
dotfeliph <feliphfeliphh@gmail.com>
2026-01-13 10:26:13 -0400

    using navigation routing

--- Home.py
+++ Home.py
@@ -1,57 +1,11 @@
 import streamlit as st
-import assets.icons as icons
-import src.utils as utils
 
-TJRO_IMAGE = "https://www.tjro.jus.br/templates/portal_tjro_helix_j5/images/presets/default/logo.png"
+pages = [
+    st.Page("src/home.py", title="Início", default=True),
+    st.Page("pages/registrar-denuncia.py", title="Registrar Denúncia", url_path="registrar-denuncia"),
+    st.Page("pages/operacional-cpcad.py", title="Operacional CPCAD", url_path="operacional-cpcad"),
+    st.Page("pages/indicadores-gestao.py", title="Indicadores de Gestão", url_path="indicadores-gestao"),
+]
 
-
-st.set_page_config(
-    page_title="Portal CPCAD", 
-    layout="centered", 
-    page_icon="⚖️", 
-    initial_sidebar_state="auto"
-)
-
-st.image(TJRO_IMAGE, width=215)
-st.title("Sistema de Gestão - CPCAD")
-st.info("Bem-vindo ao sistema de monitoramento da Comissão de Enfrentamento ao Assédio.")
-
-Atendimento, Operacional, Gestao = st.columns(3, gap="medium", vertical_alignment="top")
-
-with Atendimento:
-    with st.container(border=True, gap='medium'):
-        st.markdown(
-            utils.render_svg_icon(icons.ATENDIMENTO_DOC), 
-            unsafe_allow_html=True
-        )
-        st.subheader("Atendimento", text_alignment="center")
-        st.caption("Registro de denúncias e links públicos.")
-        if st.button("Acessar Registro", width="stretch", type="primary"):
-            st.switch_page("pages/Registrar_Denuncia.py")
-
-with Operacional:
-    with st.container(border=True, gap='medium'):
-        st.markdown(
-            utils.render_svg_icon(icons.OPERACIONAL_GEAR), 
-            unsafe_allow_html=True
-        )
-        st.subheader("Operacional", text_alignment="center" )
-        st.caption("Triagem, prazos e fluxo da comissão.")
-        if st.button("Painel Interno", width="stretch", type="primary"):
-            st.switch_page("pages/Operacional_CPCAD.py")
-
-with Gestao:
-    with st.container(border=True, gap='medium'):
-        st.markdown(
-            utils.render_svg_icon(icons.GESTAO_CHART), 
-            unsafe_allow_html=True
-        )
-        st.subheader("Gestão", text_alignment="center")
-        st.caption("Indicadores e relatórios gerenciais.")
-        if st.button("Ver Dashboard", width="stretch", type="primary"):
-            st.switch_page("pages/Indicadores_Gestao.py")
-
-st.markdown(
-    utils.footer(), 
-    unsafe_allow_html=True
-)
+pg = st.navigation(pages)
+pg.run()
